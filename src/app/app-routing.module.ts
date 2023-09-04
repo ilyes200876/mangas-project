5ab8e7ce44a231a9e5ac742ee0f24ae0d5e4f2bb
ilyes200876 <ilyes200876@live.fr>
2023-09-04 15:19:31 +0200

    componets and path creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { MangaDetailsComponent } from './manga-details/manga-details.component';
+import { MangaListComponent } from './manga-list/manga-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'mangaList', component: MangaListComponent},
+  {path: 'mangalist/:id', component: MangaDetailsComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home-component/home-component';
+import { MangaListComponent } from './manga-list/manga-list.component';
+import { MangaDetailsComponent } from './manga-details/manga-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MangaListComponent,
+    MangaDetailsComponent
   ],
   imports: [
     BrowserModule,
